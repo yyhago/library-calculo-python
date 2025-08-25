5bc0e1d76c374ea97884f627c29b74f649e4f6d3
yyhago <yyhago@vbox.educ512>
2025-08-25 09:10:54 -0300

    commit library complete

---
+++ lib/calculo.py
@@ -0,0 +1,11 @@
+def soma(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def multi(a, b):
+    return a * b
+
+def divisao(a,b):
+    return a // b
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="library_calculo",
+    version="0.0.1",
+    description="Biblioteca para realização de calulos básicos",
+    author="Yhago Felipe",
+    author_email="yhago.felipe.teles@gmail.com",
+    packages=(),
+    install_requires=[]
+)
